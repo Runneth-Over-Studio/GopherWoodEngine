95f20aaade94a718e3ea26cb3487a9fd9bb42390
Aaron Salisbury <aaron.salisbury@live.com>
2025-06-01 13:45:32 -0500

    Create initial projects.

---
+++ src/GopherWoodEngine/RunnethOverStudio.GopherWoodEngine.TestBed/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        int exitCode = 0;
+
+        try
+        {
+            //TODO:
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An unexpected error occurred: {0}", ex);
+            Console.WriteLine(ex.StackTrace);
+            exitCode = 1;
+        }
+        finally
+        {
+            Environment.Exit(exitCode);
+        }
+    }
+}
