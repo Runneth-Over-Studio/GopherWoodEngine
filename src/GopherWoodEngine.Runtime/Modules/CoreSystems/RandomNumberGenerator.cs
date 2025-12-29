af460666672810e984bc0358883988282bacbec4
Aaron Salisbury <aaron.salisbury@live.com>
2025-12-29 10:14:58 -0600

    No need to hold onto private instance of the static shared Random.

--- src/GopherWoodEngine.Runtime/Modules/CoreSystems/RandomNumberGenerator.cs
+++ src/GopherWoodEngine.Runtime/Modules/CoreSystems/RandomNumberGenerator.cs
@@ -4,95 +4,93 @@ namespace GopherWoodEngine.Runtime.Modules;
 
 internal sealed class RandomNumberGenerator : IRandomNumberGenerator
 {
-    private readonly Random _random = Random.Shared;
-
     public string GetHexString(int stringLength, bool lowercase = false)
     {
-        return _random.GetHexString(stringLength, lowercase);
+        return Random.Shared.GetHexString(stringLength, lowercase);
     }
 
     public void GetHexString(Span<char> destination, bool lowercase = false)
     {
-        _random.GetHexString(destination, lowercase);
+        Random.Shared.GetHexString(destination, lowercase);
     }
 
     public void GetItems<T>(ReadOnlySpan<T> choices, Span<T> destination)
     {
-        _random.GetItems<T>(choices, destination);
+        Random.Shared.GetItems<T>(choices, destination);
     }
 
     public T[] GetItems<T>(T[] choices, int length)
     {
-        return _random.GetItems<T>(choices, length);
+        return Random.Shared.GetItems<T>(choices, length);
     }
 
     public T[] GetItems<T>(ReadOnlySpan<T> choices, int length)
     {
-        return _random.GetItems<T>(choices, length);
+        return Random.Shared.GetItems<T>(choices, length);
     }
 
     public string GetString(ReadOnlySpan<char> choices, int length)
     {
-        return _random.GetString(choices, length);
+        return Random.Shared.GetString(choices, length);
     }
 
     public int Next()
     {
-        return _random.Next();
+        return Random.Shared.Next();
     }
 
     public int Next(int maxValue)
     {
-        return _random.Next(maxValue);
+        return Random.Shared.Next(maxValue);
     }
 
     public int Next(int minValue, int maxValue)
     {
-        return _random.Next(minValue, maxValue);
+        return Random.Shared.Next(minValue, maxValue);
     }
 
     public void NextBytes(byte[] buffer)
     {
-        _random.NextBytes(buffer);
+        Random.Shared.NextBytes(buffer);
     }
 
     public void NextBytes(Span<byte> buffer)
     {
-        _random.NextBytes(buffer);
+        Random.Shared.NextBytes(buffer);
     }
 
     public double NextDouble()
     {
-        return _random.NextDouble();
+        return Random.Shared.NextDouble();
     }
 
     public long NextInt64()
     {
-        return _random.NextInt64();
+        return Random.Shared.NextInt64();
     }
 
     public long NextInt64(long maxValue)
     {
-        return _random.NextInt64(maxValue);
+        return Random.Shared.NextInt64(maxValue);
     }
 
     public long NextInt64(long minValue, long maxValue)
     {
-        return _random.NextInt64(minValue, maxValue);
+        return Random.Shared.NextInt64(minValue, maxValue);
     }
 
     public float NextSingle()
     {
-        return _random.NextSingle();
+        return Random.Shared.NextSingle();
     }
 
     public void Shuffle<T>(T[] values)
     {
-        _random.Shuffle<T>(values);
+        Random.Shared.Shuffle<T>(values);
     }
 
     public void Shuffle<T>(Span<T> values)
     {
-        _random.Shuffle<T>(values);
+        Random.Shared.Shuffle<T>(values);
     }
 }
